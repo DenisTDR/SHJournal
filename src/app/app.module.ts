38655351879c16eab9e4e939cd1cb9aee9ab9bf3
aidadenisa <aida.denisa@gmail.com>
2017-03-11 13:11:13 +0200

    week-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import {EntryComponent} from "../components/entry-component/entry-component";
 import {EntryConfig} from "../components/entry-creator/entry-config";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {EntryTypeService} from "../services/entry-type-service";
+import {WeekPage} from "../pages/week-page/week-page";
 @NgModule({
   declarations: [
     MyApp,
     Page1,
     Page2,
     DayPage,
+    WeekPage,
     EntryComponent,
     EntryConfig
   ],
@@ -28,6 +30,7 @@ import {EntryTypeService} from "../services/entry-type-service";
     Page1,
     Page2,
     DayPage,
+    WeekPage,
     EntryConfig
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, EntryTypeService]
---
+++ src/pages/week-page/week-page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'week-page',
+  templateUrl: 'week-page.html'
+})
+export class WeekPage {
+
+  private startDay:Date;
+  private endDay:Date;
+  constructor(public navCtrl: NavController) {
+    this.startDay = new Date();
+
+  }
+
+  public endDayBuild() {
+    this.endDay = new Date(this.startDay.getTime());
+    this.endDay.setDate(this.endDay.getDate() + 7);
+
+  }
+
+}
